e7ed65f241ee76381a0c7a45c1ff9a66379ac059
nkateko <nkateko.maimane@gmail.com>
2013-08-23 14:50:42 +0200

    coded functionlaity for birthday

---
+++ development/webapp/bbdcom/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>bbd communicator</title>
+      <link rel="shortcut icon" href="images/bbd-symbol.png" >
+      <link rel="stylesheet" href="style/bootstrap.css" media="screen">
+    </head>
+	
+    <body>
+	  <div class="container">
+        <div class="page-header" id="banner">
+          <div class="row">
+            <div class="col-lg-6">
+              <h1>bbd communicator</h1>
+              <p class="lead">internal communication tool</p>
+            </div>
+          </div>
+        </div>
+		
+	  <div class="row">
+          <div class="col-lg-12">
+            <div class="well">
+              <form class="bs-example form-horizontal" action="dashboard.php">
+                <fieldset>    
+				 <legend>Login with Yammer</legend>				 
+                  <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-2">                      
+                      <button type="submit" class="btn btn-primary">Login</button> 					  
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
