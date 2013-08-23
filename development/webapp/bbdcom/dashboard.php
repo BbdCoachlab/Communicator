7d99e4b13e1372066baccdc6d67a88f193881cfc
Mabilu <snowy.mabilu@gmail.com>
2013-08-23 16:29:16 +0200

    Changed the background colour on the bbd logo so that it can blend with the web background

--- development/webapp/bbdcom/dashboard.php
+++ development/webapp/bbdcom/dashboard.php
@@ -9,7 +9,7 @@
             <div class="page-header" id="banner">
                 <div class="row">
                     <div class="col-lg-6">
-                        <h1>BBD Communicator</h1>
+                        <h1><img src="images/bbd-symbol.png" alt="bbd-logo" width="50" height="50" style="position: relative; top: -9px; ">BBD Communicator</h1>
                         <p class="lead">Internal Communication Tool</p>
                     </div>
                 </div>
@@ -19,9 +19,9 @@
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="jumbotron"style=" background-image:url(images/bday2.jpg);
-                            border:1px solid grey; 
-                            background-size: cover;
-                            background-repeat: no-repeat;">
+                             border:1px solid grey; 
+                             background-size: cover;
+                             background-repeat: no-repeat;">
                             <h1>Birthdays</h1>
                             <p>Send Birthday notifications </p>
                             <p><a class="btn btn-primary" href="birthday.php">Send Message</a></p>
@@ -43,7 +43,7 @@
                         <div class="jumbotron"style=" background-image:url(images/event2.jpg);border:1px solid grey;
                              background-size: cover;
                              background-repeat: no-repeat;">
-                            
+
                             <h1>Events</h1>
                             <p>Notify employees of events</p>
                             <p><a class="btn btn-primary" href="events.php">Send Message</a></p>
--- development/webapp/bbdcom/index.php
+++ development/webapp/bbdcom/index.php
@@ -1,64 +1,47 @@
 <!DOCTYPE html>
-  <html lang="en">
+<html lang="en">
 
     <head>      
-      <title>bbd communicator</title>
-      <link rel="shortcut icon" href="images/bbd-symbol.png" >
-      <link rel="stylesheet" href="style/bootstrap.css" media="screen">                 
+        <title>bbd communicator</title>
+        <link rel="shortcut icon" href="images/bbd-symbol.png" >
+        <link rel="stylesheet" href="style/bootstrap.css" media="screen">                 
     </head>
-	
+
     <body>   
-                
 
-	  <div class="container">
-        <div class="page-header" id="banner">
-          <div class="row">
-            <div class="col-lg-6">
 
-              <h1>BBD Communicator</h1>
-              <p class="lead">Internal Communication Tool</p>
-            </div>
-          </div>
-        </div>
-		
-	  <div class="row">
-          <div class="col-lg-12">
-            <div class="well">
-              <form class="bs-example form-horizontal" action="dashboard.php">
-                <fieldset>    
-				 <legend>Login with Yammer</legend>				 
-                  <div class="form-group">
-                    <div class="col-lg-10 col-lg-offset-2">                      
-                      <button type="submit" class="btn btn-primary">Login</button> 					  
+        <div class="container">
+
+            <div class="page-header" id="banner">	
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h1><img src="images/bbd-symbol.png" alt="bbd-logo" width="50" height="50" style="position: relative; top: -9px; ">BBD Communicator</h1>
+                        <p class="lead">Internal Communication Tool</p>
+
                     </div>
-                  </div>
-				</fieldset>
-              <h1><img src="images/bbd-symbol.png" alt="bbd-logo" width="50" height="50" style="position: relative; top: -9px; ">BBD Communicator</h1>
-              <p class="lead">Internal Communication Tool</p>
+                </div>
+            </div>                   
+            <div class="row">
+                <div class="col-lg-9">            
+                    <div class="jumbotron">
+                        <h1>Efficient Communication</h1>
+                        <p>The BBD Communicator makes it possible for efficient internal 
+                            communications.
+                        </p>              
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="well">
+                        <form class="bs-example form-horizontal">
+                            <fieldset>    
+                                <legend>Login with Yammer</legend>	
+                                <input type="button" class="btn btn-primary btn-lg" onClick="window.location = 'https://www.yammer.com/dialog/oauth?client_id=nEjhbfN94g3w2nAYczxEw&redirect_uri=http://localhost/bbdcom/scripts/login.php';" value="Login"> 
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>                   
-	  <div class="row">
-           <div class="col-lg-9">            
-            <div class="jumbotron">
-              <h1>Efficient Communication</h1>
-              <p>The BBD Communicator makes it possible for efficient internal 
-                  communications.
-              </p>              
-            </div>
-          </div>
-          <div class="col-lg-3">
-            <div class="well">
-              <form class="bs-example form-horizontal">
-                <fieldset>    
-	          <legend>Login with Yammer</legend>	
-                  <input type="button" class="btn btn-primary btn-lg" onClick="window.location ='https://www.yammer.com/dialog/oauth?client_id=nEjhbfN94g3w2nAYczxEw&redirect_uri=http://localhost/bbdcom/scripts/login.php';" value="Login"> 
-		</fieldset>
-              </form>
-            </div>
-          </div>
         </div>
-      </div>
 
     </body>
-  </html>
+</html>
