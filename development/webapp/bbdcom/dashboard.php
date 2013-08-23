562d4cc53e1d720347c000de491db61f624500f7
Mabilu <snowy.mabilu@gmail.com>
2013-08-23 13:50:09 +0200

    Created the dashboard and modified the css template to complemnt the dashboard design

---
+++ development/webapp/bbdcom/dashboard.php
@@ -0,0 +1,55 @@
+<?php include('menu.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <body>
+        <div class="container">
+            <div class="page-header" id="banner">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h1>BBD Communicator</h1>
+                        <p class="lead">Internal Communication Tool</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bs-docs-section">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="jumbotron"style=" background-image:url(images/bday2.jpg);
+                            border:1px solid grey; 
+                            background-size: cover;
+                            background-repeat: no-repeat;">
+                            <h1>Birthdays</h1>
+                            <p>Send Birthday notifications </p>
+                            <p><a class="btn btn-primary" href="birthday.php">Send Message</a></p>
+                        </div>            
+                    </div>
+
+                    <div class="col-lg-4">            
+                        <div class="jumbotron"style=" background-image:url(images/award2.jpg);
+                             border:1px solid grey;
+                             background-size: cover;
+                             background-repeat: no-repeat; ">
+                            <h1>Awards</h1>
+                            <p>Remind employees to nominate</p>
+                            <p><a class="btn btn-primary " href="awards.php">Send Message</a></p>
+                        </div>         
+                    </div>
+
+                    <div class="col-lg-4">            
+                        <div class="jumbotron"style=" background-image:url(images/event2.jpg);border:1px solid grey;
+                             background-size: cover;
+                             background-repeat: no-repeat;">
+                            
+                            <h1>Events</h1>
+                            <p>Notify employees of events</p>
+                            <p><a class="btn btn-primary" href="events.php">Send Message</a></p>
+                        </div>
+                    </div>          
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ development/webapp/bbdcom/menu.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>bbd communicator</title>
+      <link rel="shortcut icon" href="images/bbd-symbol.png" >
+      <link rel="stylesheet" href="style/bootstrap.css" media="screen">
+    </head>
+	
+    <body>
+      <div class="navbar navbar-fixed-top">
+        <div class="container">
+          <!--<a href="dashboard.php" class="navbar-brand">bbd communicator</a>-->
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="nav-collapse collapse" id="navbar-main">
+            <ul class="nav navbar-nav">
+              <li><a href="dashboard.php">Dashboard</a></li>
+              <li><a href="birthday.php">Birthday Messages</a></li>
+              <li><a href="awards.php">Awards</a></li>
+              <li><a href="events.php">Events</a></li>
+		    </ul>
+			
+            <ul class="nav navbar-nav pull-right">
+              <li><a href="index.php" >Log out</a></li>            
+            </ul>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
