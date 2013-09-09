46f31c2132990a8bb90a2a9d95f160920a3afd80
asheensingh <sheensin@gmail.com>
2013-09-09 17:04:27 +0200

    Added validation to the forms

---
+++ development/webapp/bbdcom/js/basic_form_validation.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {    
+    $("#basic_form").validate({
+        rules: {
+            subject: {
+                required: true
+            },
+            department_list: {
+                required:true
+            }
+        },
+        messages: {
+            subject: {
+                required:"Please provide a subject"
+            },
+            department_list: {
+                required: "Please select recipients of message"
+            }
+        },
+        highlight: function (element, errorClass) {
+            $(element).removeClass(errorClass);                        
+            $(element).parent().addClass("form-group has-error");
+        }
+    });
+
+});
