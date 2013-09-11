271ce2a51a62def79b56ed609f58263d7308eb32
BandileN <bandilendlela@gmail.com>
2013-09-11 09:20:59 +0200

    removed php scripts folder for renaming

--- development/PHPWebProject1/PHPWebProject1/Department_Note_UserScript.php
+++
@@ -1,7 +0,0 @@
-<?php
-//add link between user, department, and note
-//delete 3-way link or delete all received notifications
-//get all notifications for a specific user.
-//get all users a specific notifiaction was sent to.
-//received: update received count.
-?>
--- development/PHPWebProject1/PHPWebProject1/Department_UserScript.php
+++
@@ -1,6 +0,0 @@
-<?php
-//add department user link: increase department count
-//delete department user link
-//fetch all groups linked with user
-//fetch all users linked with group
-?>
