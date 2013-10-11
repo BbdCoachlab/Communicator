86de971da9ee8b8510d95b5874887b7bd318dcd2
tshepiso <tshepiso.mothlele@gmail.com>
2013-10-11 07:15:59 +0200

    Added the comments

--- development/MobileApps/Server side for mobileApps/PHPWebProject1/PHPWebProject1/index.php
+++ development/MobileApps/Server side for mobileApps/PHPWebProject1/PHPWebProject1/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html> <!-- Create Questionaire form events-->>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
